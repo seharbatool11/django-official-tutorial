7a293f777022aad7cb41331a0856ee3d7851c8b3
Sehar Batool <sehar.quadiery@gmail.com>
2023-07-10 10:36:13 +0500

    First commit

---
+++ polls/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+
+from django.template import loader
+from .models import Question
+   
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    data = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(data, request))
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question{ question_id}.")
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
